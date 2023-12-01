2b45747e9ac1c613160fddf3bc9c1f23eaaa41f7
rl-subscription <149235011+rl-subscription@users.noreply.github.com>
2023-12-02 03:44:48 +0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+--[[ ===================================================== ]]--
+--[[          QBCore Job Garage Menu Script by RL          ]]--
+--[[ ===================================================== ]]--
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'RL-Subscription'
+description 'RL-Garage Menu with categories'
+version '1.0.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/nl.lua', -- Change this to your preferred language
+    'config.lua',
+}
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client/main.lua',
+}
+
+server_scripts {
+    'server/update.lua',
+}
+
+lua54 'yes'
